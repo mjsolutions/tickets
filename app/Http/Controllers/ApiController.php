a6918281a8653b5e2d3e859ecfcb0d2668965a88
JimmyRdzS <jaime_rdez794@live.com.mx>
2017-05-13 14:54:21 -0500

    Cambios paypal guanajuato y celaya

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -12,18 +12,26 @@ class ApiController extends Controller
 {
     public function getFilas($table, $zona) {
         if($table == "Franco6"){
-    	   return Franco6::select('fila')->where([['seccion', $zona], ['status', 0]])->groupBy('fila')->get();
+            return Franco6::select('fila')->where([['seccion', $zona], ['status', 0]])->groupBy('fila')->get();
         }elseif($table == "Franco_sma"){
             return Franco_sma::select('fila')->where([['seccion', $zona], ['status', 0]])->groupBy('fila')->get();
+        }elseif($table == "Franco_celaya"){
+            return Franco_celaya::select('fila')->where([['seccion', $zona], ['status', 0]])->groupBy('fila')->get();
+        }elseif($table == "Franco_guanajuato"){
+            return Franco_guanajuato::select('fila')->where([['seccion', $zona], ['status', 0]])->groupBy('fila')->get();
         }
         return Franco7::select('fila')->where([['seccion', $zona], ['status', 0]])->groupBy('fila')->get();
     }
 
     public function getAsientos($table, $fila) {
         if($table == "Franco6"){
-    	   return Franco6::select('id','asiento')->where([['fila', $fila], ['status', 0]])->get();   
+            return Franco6::select('id','asiento')->where([['fila', $fila], ['status', 0]])->get();   
         }elseif($table == "Franco_sma"){
             return Franco_sma::select('id','asiento')->where([['fila', $fila], ['status', 0]])->get();
+        }elseif($table == "Franco_celaya"){
+            return Franco_celaya::select('id','asiento')->where([['fila', $fila], ['status', 0]])->get();
+        }elseif($table == "Franco_guanajuato"){
+            return Franco_guanajuato::select('id','asiento')->where([['fila', $fila], ['status', 0]])->get();
         }
         return Franco7::select('id','asiento')->where([['fila', $fila], ['status', 0]])->get();
     }
